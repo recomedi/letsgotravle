c5ce4b546981d5dec66964e122bf805806c2de92
biso15 <biso150@gmail.com>
2025-02-16 19:48:03 +0900

    일정 상세 중간 커밋

--- letsgotravel/src/main/java/com/letsgotravel/myapp/controller/ScrapController.java
+++ letsgotravel/src/main/java/com/letsgotravel/myapp/controller/ScrapController.java
@@ -3,6 +3,7 @@ package com.letsgotravel.myapp.controller;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,12 +26,18 @@ public class ScrapController {
 //	private ScrapVo sv;
 	
 	@RequestMapping(value = "scrapWriteAction.do", method = RequestMethod.POST)
-	public String scrapWriteAction(@RequestParam("calendarData") String calendarData) {
+	public String scrapWriteAction(
+			@RequestParam("calendarData") String calendarData,
+			@RequestParam("sights") String sights,
+			@RequestParam("restaurants") String restaurants,
+			@RequestParam("destination") String destination,
+			Model model) {
 		
 		logger.info("scrapWriteAction들어옴");
 
 		//int value = scrapService.scrapInsert(sv);
 		logger.info("calendarData:" + calendarData);
+	    
 //
 //		String path = "";
 //		if (value == 1) {
@@ -38,6 +45,9 @@ public class ScrapController {
 //		} else if (value == 0) {
 //			path = "redirect:/member/memberSignup.do";
 //		}
+
+		model.addAttribute("destination", destination);
+		model.addAttribute("calendarData", calendarData);
 		
 		return "WEB-INF/travel/travelDetails";
 	}
