2f7c3c80e5f13ece0c2834e1b83e3c2a2a3c1f08
biso15 <biso150@gmail.com>
2025-02-18 01:53:30 +0900

    ggw 브랜치 커밋

--- letsgotravel/src/main/java/com/letsgotravel/myapp/controller/ScrapController.java
+++ letsgotravel/src/main/java/com/letsgotravel/myapp/controller/ScrapController.java
@@ -5,48 +5,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.letsgotravel.myapp.util.UserIp;
+import javax.servlet.http.HttpSession; // 🔹 세션을 사용하기 위해 추가
+import com.letsgotravel.myapp.domain.Criteria;
+import com.letsgotravel.myapp.domain.PageMaker;
+import com.letsgotravel.myapp.service.ScrapService;
+import org.slf4j.Logger;
 
 @Controller
 @RequestMapping(value = "/scrap")
 public class ScrapController {
-		
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ScrapController.class);
-	
-	@Autowired(required=false)
-	private UserIp userip;
-	
-//	@Autowired
-//	private ScrapService scrapService;
 
-//	@Autowired
-//	private ScrapVo sv;
-	
-	@RequestMapping(value = "scrapWriteAction.do", method = RequestMethod.POST)
-	public String scrapWriteAction() {
-		logger.info("scrapWriteAction들어옴");
+    private static final Logger logger = LoggerFactory.getLogger(ScrapController.class);
 
-		//int value = scrapService.scrapInsert(sv);	    
-//
-//		String path = "";
-//		if (value == 1) {
-//			path = "redirect:/";
-//		} else if (value == 0) {
-//			path = "redirect:/member/memberSignup.do";
-//		}
-		
-		return "WEB-INF/scrap/scrapList";
-	}
+    @Autowired
+    private ScrapService scrapService;
 
-	@RequestMapping(value = "scrapList.do")
-	public String scrapList() {
-		logger.info("scrapList들어옴");
+    @Autowired
+    private PageMaker pageMaker;
 
-		return "WEB-INF/scrap/scrapList";
-	}
-	
-	
+    @RequestMapping(value = "/scrapList.do")
+    public String scrapList(HttpSession session, Criteria cri, Model model) {
+        Integer midx = (Integer) session.getAttribute("midx"); // 🔹 세션에서 midx 가져오기
+
+        if (midx == null) {  //로그인 안 했으면 로그인 페이지로 이동
+            logger.warn("로그인이 필요합니다.");
+            return "redirect:/login.do"; 
+        }
+
+        logger.info("scrapList 실행됨! midx: " + midx + ", page: " + cri.getPage());
+
+        // 특정 사용자의 스크랩 목록 가져오기
+        model.addAttribute("scrapList", scrapService.getScrapList(midx, cri));
+
+        int totalCount = scrapService.scrapTotalCount(midx);
+        pageMaker.setCri(cri);
+        pageMaker.setTotalCount(totalCount);
+        model.addAttribute("pageMaker", pageMaker);
+
+        return "WEB-INF/scrap/scrapList";
+    }
 }
--- letsgotravel/src/main/java/com/letsgotravel/myapp/domain/ScrapVo.java
+++ letsgotravel/src/main/java/com/letsgotravel/myapp/domain/ScrapVo.java
@@ -1,77 +1,77 @@
 package com.letsgotravel.myapp.domain;
 
 public class ScrapVo {
-		
-	public int pidx;
-	public int midx;
-	public String resMenufactureDate;
-	public String resPrescribeOrg;
-	public String resTelNo;
-	public String commBrandName;
-	public String commTelNo;
-	public String date;
-	public String ip;
-	public String delyn;
-	
-	public int getPidx() {
-		return pidx;
-	}
-	public void setPidx(int pidx) {
-		this.pidx = pidx;
-	}
-	public int getMidx() {
-		return midx;
-	}
-	public void setMidx(int midx) {
-		this.midx = midx;
-	}
-	public String getResMenufactureDate() {
-		return resMenufactureDate;
-	}
-	public void setResMenufactureDate(String resMenufactureDate) {
-		this.resMenufactureDate = resMenufactureDate;
-	}
-	public String getResPrescribeOrg() {
-		return resPrescribeOrg;
-	}
-	public void setResPrescribeOrg(String resPrescribeOrg) {
-		this.resPrescribeOrg = resPrescribeOrg;
-	}
-	public String getResTelNo() {
-		return resTelNo;
-	}
-	public void setResTelNo(String resTelNo) {
-		this.resTelNo = resTelNo;
-	}
-	public String getCommBrandName() {
-		return commBrandName;
-	}
-	public void setCommBrandName(String commBrandName) {
-		this.commBrandName = commBrandName;
-	}
-	public String getCommTelNo() {
-		return commTelNo;
-	}
-	public void setCommTelNo(String commTelNo) {
-		this.commTelNo = commTelNo;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getIp() {
-		return ip;
-	}
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-	public String getDelyn() {
-		return delyn;
-	}
-	public void setDelyn(String delyn) {
-		this.delyn = delyn;
-	}
-	
+    
+    public int sidx;
+    public int tcidx;
+    public String destination;
+    public int duration;
+    public int peopleCount;
+    public String budget;
+    public String groupType;
+    public String thema;
+    public String date;
+
+    public int getSidx() {
+        return sidx;
+    }
+    public void setSidx(int sidx) {
+        this.sidx = sidx;
+    }
+
+    public int getTcidx() {
+        return tcidx;
+    }
+    public void setTcidx(int midx) {
+        this.tcidx = midx;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getPeopleCount() {
+        return peopleCount;
+    }
+    public void setPeopleCount(int peopleCount) {
+        this.peopleCount = peopleCount;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getGroupType() {
+        return groupType;
+    }
+    public void setGroupType(String groupType) {
+        this.groupType = groupType;
+    }
+
+    public String getThema() {
+        return thema;
+    }
+    public void setThema(String thema) {
+        this.thema = thema;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
